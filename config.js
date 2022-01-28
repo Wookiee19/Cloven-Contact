087beeff770a20537c215ba3660dfa78acf526d5
Wookiee19 <93146509+Wookiee19@users.noreply.github.com>
2022-01-28 11:56:38 +0530

    aws config

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    aws_table_name: 'dynamodb-contact',
+    aws_local_config: {
+      //Provide details for local configuration
+    },
+    aws_remote_config: {
+      accessKeyId: 'AKIA6AP3LOSFWBS7ZOLA',
+      secretAccessKey: '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',
+      region: 'ap-south-1',
+    }
+};
